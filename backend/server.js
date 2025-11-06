ffa07f54a01d95f9b071ed80863711e8470e1db1
SachiniAlahakoon <sachinialahakoon327.com>
2025-11-06 22:11:31 +0530

    Initial commit with backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// simple route
+app.get('/', (req, res) => res.send('Student Management Backend running'));
+
+// routes
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+// connectDB();
