82e83d505c40c1f5c7123b38d35417ef6e708678
chathuranga-dew <chathurangalakshan555@gmail.com>
2025-12-20 16:41:08 +0530

    Add TopBar images folderand SideBar.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import TopBar from './components/TopBar.jsx'
+import SideBar from './components/SideBar.jsx';
+// import StudentDashboard from './pages/StudentDashboard.jsx'
+// import AdminDashboard from './pages/AdminDashboard.jsx';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+    <div>
+      <TopBar />
+    </div>
+    <div>
+      <SideBar />
+    </div>
+      <main className="react-router-dom">
+        {/* <Routes>
+          <Route path="/student-dashboard" element={<StudentDashboard />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />}/>
+        </Routes> */}
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/SideBarData.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import CampaignIcon from '@mui/icons-material/Campaign';
+
+export const SideBarData = [
+    {
+        title: "Profile",
+        icon: <AccountCircleIcon />,
+        link: "/profile"
+    },
+    {
+        title: "Exam Results",
+        icon: <BarChartIcon />,
+        link: "/exam-results"
+    },
+    {
+        title: "Notices",
+        icon: <CampaignIcon />,
+        link: "/notices"
+    }
+]
